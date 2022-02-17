e1c4333f1e5e4d9a602623d3ffb5f99f4b113829
DanVortex <danielvortex@hotmail.com>
2022-02-17 09:23:43 -0300

    requisito 17

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,15 +1,31 @@
 import React from 'react';
-// import Header from '../Components/Header';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  btnRanking = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
 
         <h1 data-testid="ranking-title">Ranking page</h1>
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ this.btnRanking }
+        >
+          Home
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
 
 export default Ranking;
